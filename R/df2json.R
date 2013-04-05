08c7bf5a08cc8128fc9ee5d125925abdcbc9f40c
Nacho Caballero <nachocab@gmail.com>
2013-04-05 00:00:00 +0000

    version 0.0.2

--- R/df2json.R
+++ R/df2json.R
@@ -34,19 +34,53 @@ prepare_for_JSON <- function(data){
 }
 
 
-#' Convert a dataframe to JSON
+#' Convert a data frame to JSON
 #'
+#' It returns a string that contains an array of objects. There are as many JS objects as there are rows in the data frame. The column names are used as the keys of each object. The objects are separated by newlines.
 #'
-#' @param data input dataframe object
+#' @param df input dataframe object
 #' @export
 #' @examples library(df2json)
 #' df <- data.frame(name=c("a", "b", "c"), x=c(NA, 2 ,3), y=c(10, 20, -Inf), show=c(TRUE, FALSE, TRUE))
 #' df2json(df)
-df2json <- function(data){
-    data <- prepare_for_JSON(data)
-    objects <- apply(data, 1, function(row) {paste(row, collapse = ',')})
+df2json <- function(df){
+    df <- prepare_for_JSON(df)
+    objects <- apply(df, 1, function(row) {paste(row, collapse = ',')})
     objects <- paste0('{', objects, '}')
     objects <- paste0('[', paste(objects, collapse = ',\n'), ']')
 
     objects
+}
+
+#' Convert a matrix to JSON
+#'
+#' It returns a string of nested arrays. One array per row.
+#'
+#' @param mat input matrix object
+#' @export
+#' @import rjson
+#' @examples library(df2json)
+#' df <- matrix(1:9, byrow = TRUE, nrow=3)
+#' matrix2json(df)
+matrix2json <- function(mat){
+    mat <- as.list(as.data.frame(t(mat)))
+    names(mat) <- NULL
+    json <- toJSON(mat)
+    json
+}
+
+#' Convert a JSON string into a data frame
+#'
+#' @param json input json object
+#' @export
+#' @import rjson
+#' @examples library(df2json)
+#' json <- "[{\"a\":1, \"b\":2},{\"a\":3,\"b\":4}]"
+#' json2df(json)
+json2df <- function(json){
+    json <- fromJSON(json)
+    df <- do.call(rbind.data.frame, json)
+    rownames(df) <- NULL
+
+    df
 }
